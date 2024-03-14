33d479878bb2d9ecce1b8da8112c0cac9d5ccd7c
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-14 17:14:08 +0530

    env Variables Added

---
+++ src/Auth/fireBaseConfig.tsx
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import dotenv from "dotenv"
 
 export default defineConfig({
   plugins: [react()],
+  define:{
+   'process.env.VITE_apiKey' : JSON.stringify(process.env.VITE_apiKey),
+   'process.env.VITE_authDomain': JSON.stringify(process.env.VITE_apiKey),
+   'process.env.VITE_projectId': JSON.stringify(process.env.VITE_apiKey),
+   'process.env.VITE_storageBucket':JSON.stringify(process.env.VITE_apiKey),
+   'process.env.VITE_messagingSenderId': JSON.stringify(process.env.VITE_apiKey),
+   'process.env.VITE_appId': JSON.stringify(process.env.VITE_apiKey),
+   'process.env.VITE_measurementId': JSON.stringify(process.env.VITE_apiKey),
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
