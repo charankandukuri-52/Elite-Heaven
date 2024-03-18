580586321add5e30a3f7f6235abe949c5873c599
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-18 19:54:16 +0530

    Updated Routes according to Filters

--- src/Routes/hotelRoutes.js
+++ src/Routes/hotelRoutes.js
@@ -1,9 +1,20 @@
 // routes/hotelRoutes.js
 import express from 'express';
-import { getHotelData } from '../controllers/hotelController.js';
+import {getHotelData, filterByPropertyType,getHotelDataByCity, getHotelDataByNeighborhood, getHotelDataByDistance,filterByGuests, filterByBedrooms, filterByBeds, filterByBathrooms, getHotelDataByGuestsIncluded,filterByCheckInOutDates, filterByAvailabilityDates } from '../Controllers/hotelController.js';
 
 const router = express.Router();
 
 router.get('/hotels', getHotelData);
+router.get('/accommodations/city', getHotelDataByCity);
+router.get('/accommodations/neighborhood', getHotelDataByNeighborhood);
+router.get('/accommodations/distance', getHotelDataByDistance);
+router.get('/accommodations/guests_included', getHotelDataByGuestsIncluded);
+router.get('/accommodations/checkInOut', filterByCheckInOutDates);
+router.get('/accommodations/availability', filterByAvailabilityDates);
+router.get('/accommodations', filterByPropertyType);
+router.get('/accommodations/guests', filterByGuests);
+router.get('/accommodations/bedrooms', filterByBedrooms);
+router.get('/accommodations/beds', filterByBeds);
+router.get('/accommodations/bathrooms', filterByBathrooms);
 
 export default router;
