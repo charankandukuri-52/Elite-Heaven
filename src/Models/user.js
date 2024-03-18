86e799952c3bde8eed15b406d4c99da681add4d3
charankandukuri-52 <c2h4a1r2@gmail.com>
2024-03-18 19:19:43 +0530

    Defined User Model

---
+++ src/Models/user.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'manager', 'customer'],
+        default: 'customer'
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
